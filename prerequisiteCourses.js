a9375b2e98b95e72273bf5f66f13cb5a823471b6
Lucas Mazo <lucasmazo32@gmail.com>
2020-07-14 11:25:08 -0500

    added graph problem

---
+++ prerequisiteCourses.js
@@ -0,0 +1,16 @@
+const canFinish = function(numCourses, prerequisites) {
+  const obj = {};
+  prerequisites.forEach(prerequisite => {
+      if (obj[prerequisite[0]]) {
+          obj[prerequisite[0]].push(prerequisite[1]);
+      } else {
+          obj[prerequisite[0]] = [prerequisite[1]];
+      }
+  });
+  const done = [];
+  for(let i = 0; i < numCourse; i++) {
+      if (obj[i] === null) {
+          done.push(i);
+      }
+  }
+};
